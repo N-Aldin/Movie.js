77496c8bd2edbf4340ac409fab82de90bca57744
root <root@Aldins-Laptop.localdomain>
2021-10-11 18:38:57 -0400

    UI changes

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,38 +1,47 @@
 import React from 'react';
 
-import {Card, CardHeader, CardActions, CardMedia, IconButton, Typography} from "@material-ui/core";
+import {Card, CardHeader, CardActions, CardMedia, IconButton, Typography, CardActionArea} from "@material-ui/core";
 import StarIcon from '@mui/icons-material/Star';
 import MovieIcon from '@mui/icons-material/Movie';
+import WebIcon from '@mui/icons-material/Language';
 import Styled from 'styled-components';
 
 let Movie = ({ movie }) => (
   <MovieCard>
     {console.log(movie)}
+    <CardActionArea>
+
     <CardHeader
-      action={
-        <IconButton >
-          <MovieIcon />
-        </IconButton>
-      }
       title={<MovieTitle noWrap>{movie.title}</MovieTitle>}
       subheader={movie.release_date}
     />
     <Poster
       component="img"
-      image={"https://image.tmdb.org/t/p/w342" + movie.poster_path}
+      image={"https://image.tmdb.org/t/p/w185" + movie.poster_path}
     />
     <CardActions>
-      <IconButton>
+      {/* <IconButton style={{backgroundColor: "transparent", padding: "12px 5px"}}> */}
+      {/* <IconButton style={{ "&:hover": { backgroundColor: "transparent" }} } aria-label="Delete"> */}
+      <IconContainer>
         <StarIcon/>
         <Typography>{movie.vote_average}</Typography>
-      </IconButton>
+      </IconContainer>
+      {/* </IconButton> */}
     </CardActions>
+    </CardActionArea>
+
   </MovieCard>
 );
 
+const IconContainer = Styled.div`
+  display: flex;
+  padding: 12px 5px;
+  color: grey;
+`;
+
 const MovieCard = Styled(Card)`
-  width: 250px;
-  height: 394px;
+  width: 185px;
+  // height: 394px;
   // height: auto;
   margin: auto;
   transition: transform 0.25s;
@@ -43,14 +52,14 @@ const MovieCard = Styled(Card)`
 `;
 
 const MovieTitle = Styled(Typography)`
-  max-width: 178px;
+  // max-width: 178px;
+  max-width: 165px;
   font-weight: 600;
 `;
 
 const Poster = Styled(CardMedia)`
-  margin: auto;
-  width: 250px;
-  height: 250px;
+  width: 100%;
+  height: 100%;
 `;
 
 export default Movie;
