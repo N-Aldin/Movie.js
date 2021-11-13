e77f7d47f2545573e50bc26ac4a8494dce02780c
root <aldin.newaz@outlook.com>
2021-11-13 18:55:05 -0500

    Added pagination

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import dotenv from "dotenv";
 import "./App.css";
 import React, { useState, useEffect } from 'react';
 import { Typography } from '@material-ui/core';
+import { Pagination } from '@material-ui/lab'
 import Styled from 'styled-components';
 import axios from "axios";
 import NavBar from "./Components/NavBar";
 import MovieGrid from './Components/MovieGrid';
-import MovieInfo from "./Components/MovieInfo";
-
-dotenv.config();
+import MovieInfo from './Components/MovieInfo';
+import MoviePagination from './Components/MoviePagination';
 
 function App() {
 
   const [data, setData] = useState([]);
   const [movieDisplay, setMovieDisplay] = useState(0);
+  const [page, setPage] = useState(1);
 
   // Fetch data on initial render
   useEffect(() => {
-    axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=" + process.env.REACT_APP_API_KEY + "&language=en-US&page=1")
+    axios.get("https://api.themoviedb.org/3/movie/top_rated?api_key=" + process.env.REACT_APP_API_KEY + "&language=en-US&page=" + page)
       .then(res => {
         setData(res.data.results)
       }).catch(err => console.log(err));
-  }, []);
+  }, [page]);
+
+  const handlePagination = (e, page) => {
+    setPage(page);
+    // console.log(page)
+  };
 
   // This prints twice because of the React strictmode
   // UseEffect still only runs once so everything else here is functional
@@ -34,6 +39,8 @@ function App() {
       <MovieGrid movie={data} movieID={setMovieDisplay} />
 
       {movieDisplay && <MovieInfo movieID={movieDisplay} setMovieDisplay={setMovieDisplay} />}
+
+      <MoviePagination handlePagination={handlePagination} pageCount={page} />
     </>
   );
 }
---
+++ src/Components/MoviePagination.js
@@ -0,0 +1,20 @@
+import react from 'react';
+import { Pagination } from '@material-ui/lab';
+import Styled from 'styled-components';
+
+const MoviePagination = ({ handlePagination, pageCount }) => {
+
+  return (
+    <Container>
+      <Pagination count={Math.max(15, pageCount + 1)} onChange={handlePagination} />
+    </Container>
+  );
+}
+
+const Container = Styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+`;
+
+export default MoviePagination;
